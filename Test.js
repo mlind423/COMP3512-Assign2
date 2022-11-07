9c0a13423f71953c64c973a877adf21a467e2320
mlind423 <77463748+mlind423@users.noreply.github.com>
2022-11-07 15:42:53 -0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,6 @@
 //Since api.js is module we can use it in every JS file that need 
 import * as data from "./api.js";
 
+
+//data.retrieveStorage(); might not be the best way to grab the information since it might load nothing if the api has not been downloaded 
+
