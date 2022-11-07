b7b1acc989ae1f7435f087c6d969dffdd965f811
mlind423 <77463748+mlind423@users.noreply.github.com>
2022-11-07 15:39:24 -0700

    api data handling
    I got the data from the api loading into local storage on launch and not loading from the api if it exists in local storage. The only issue is if we try to use the info in local storage before its loaded from the api then nothing will happen.

---
+++ Test.js
@@ -0,0 +1,3 @@
+//Since api.js is module we can use it in every JS file that need 
+import * as data from "./api.js";
+
--- api.js
+++ api.js
@@ -1,8 +1,50 @@
 /* url of song api --- https versions hopefully a little later this semester */	
-const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+// I just commented this out to prevent overuse of the api
+//const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+document.addEventListener('DOMContentLoaded', () => {
+   
+   storedata();
+   /* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
+      local file). To work correctly, this needs to be tested on a local web server.  
+      Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+      use built-in Live Preview.
+   */
+   
+      /*
+      This function checks to see if the data is already in local storage or not 
+      if it isnt then load from the api and store it in localStorage
+      */
+      function storedata(){
+         let test = false;
+         if(localStorage.getItem('data') === null){ // idk why i need to check if it contains undefined and not that its undefined.
+            document.querySelector('body').style.display = 'none'; // will hide everything until things are loaded form the api
+            console.warn("loading from API");
+            getData().then((e) => {
+               localStorage.setItem('data', JSON.stringify(e));
+               document.querySelector('body').style.display = ''; //will unhide everything once things are loaded 
+            });
+         }else{
+            console.log("information already in local storage");
+         }
 
-/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.  
-   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-   use built-in Live Preview.
-*/
+      }
+   
+      function retrieveStorage() {        
+         return JSON.parse(localStorage.getItem('data')) || null;
+      }
+   
+   
+   
+   
+   async function getData(){
+      const resp = await fetch(api);
+      const data = await resp.json();
+      return data;
+   }
+   
+  
+   
+   
+});
+
+export{retrieveStorage};
