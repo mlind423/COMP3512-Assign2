ad1c14a1f400849fe5bfe052d4b56865a965f32b
mlind423 <77463748+mlind423@users.noreply.github.com>
2022-11-15 09:37:44 -0700

    Did some work

--- Main.js
+++ Main.js
@@ -9,9 +9,17 @@ document.addEventListener("DOMContentLoaded", function(){
             const opt = document.createElement('option');
             opt.textContent = a.name;
             opt.setAttribute('value', a.id);
+            //opt.setAttribute('id', a.name);
             document.querySelector(`#${id}`).appendChild(opt);
         }
     }
-    
+
+    document.querySelector('#artist').addEventListener('input', (e) =>{
+        console.log(e.target.value); //Gives the value of the artist that is clicked
+    });
+    document.querySelector('#genre').addEventListener('input', (e) => {
+        console.log(e.target.value); //Gives the value of the genre that was clicked
+    });
+
 });
 
