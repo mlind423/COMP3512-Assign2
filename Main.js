447632ef325335617e085fe3f543049a9e311a23
Spencerreid559 <sreid559@mtroyal.ca>
2022-11-16 11:19:00 -0700

    Implemented Table and some Organization
    Co-Authored-By: mlind423 <77463748+mlind423@users.noreply.github.com>

--- Main.js
+++ Main.js
@@ -1,11 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
     const artists = JSON.parse(artist);
     const genres = JSON.parse(genre);
+    const data = JSON.parse(localStorage.getItem("data"));
     createOpt("artist", artists);
     createOpt("genre", genres);
-
-    function createOpt(id, data){
-        for(a of data){
+    function addTableData(trElement, tdData){
+        trElement.setAttribute("data-songID", tdData.song_id);
+        let td = document.createElement("td");
+        td.textContent = tdData.title;
+        trElement.appendChild(td);
+        td = document.createElement("td");
+        td.textContent = tdData.artist.name;
+        trElement.appendChild(td);
+        td = document.createElement("td");
+        td.textContent = tdData.year;
+        trElement.appendChild(td);
+        td = document.createElement("td");
+        td.textContent = tdData.genre.name;
+        trElement.appendChild(td);
+        td = document.createElement("td");
+        td.textContent = tdData.details.popularity;
+        trElement.appendChild(td);
+        const button = document.createElement("button");
+        button.type = "button";
+        button.textContent = "Add";
+        button.setAttribute("value", tdData.song_id);
+        trElement.appendChild(button);
+    }
+    function createOpt(id, optionData){
+        for(a of optionData){
             const opt = document.createElement('option');
             opt.textContent = a.name;
             opt.setAttribute('value', a.id);
@@ -13,13 +36,19 @@ document.addEventListener("DOMContentLoaded", function(){
             document.querySelector(`#${id}`).appendChild(opt);
         }
     }
-
-    document.querySelector('#artist').addEventListener('input', (e) =>{
-        console.log(e.target.value); //Gives the value of the artist that is clicked
-    });
-    document.querySelector('#genre').addEventListener('input', (e) => {
-        console.log(e.target.value); //Gives the value of the genre that was clicked
-    });
-
-});
-
+    for (let a of data){
+        let tr = document.createElement("tr");
+        addTableData(tr,a);
+        document.querySelector("#songs").appendChild(tr);
+    }
+    /* document.querySelector("#tableHeader").addEventListener("click", (e) =>{
+        for (let element of data){
+            element.sort((a,b) =>{
+                if (a.title < b.title) {return -1};
+                if (a.title > b.title) {return 1};
+                return 0;
+            });
+            console.log(element);
+        }
+    }); */
+}); 
